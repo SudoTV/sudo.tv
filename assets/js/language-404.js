89daa1624d9cb9fc5adad7cdb3ee9d5b0f90e56f
WMXPY <wm@mengw.io>
2021-12-19 18:50:26 -0600

    implement 4040 redirect

---
+++ assets/js/language-404.js
@@ -0,0 +1,17 @@
+const language = window.navigator.userLanguage || window.navigator.language;
+
+(() => {
+
+    if (
+        window.location.pathname.startsWith('/zh')
+        || window.location.pathname.startsWith('/en')
+    ) {
+        return;
+    }
+
+    if (language === 'zh-CN') {
+        window.location.href = `/zh${window.location.pathname}`;
+    } else {
+        window.location.href = `/en${window.location.pathname}`;
+    }
+})();
--- assets/js/language-index.js
+++ assets/js/language-index.js
@@ -1,7 +1,10 @@
 const language = window.navigator.userLanguage || window.navigator.language;
 
-if (language === 'zh-CN') {
-    window.location.href = '/zh';
-} else {
-    window.location.href = '/en';
-}
+(() => {
+
+    if (language === 'zh-CN') {
+        window.location.href = '/zh';
+    } else {
+        window.location.href = '/en';
+    }
+})();
