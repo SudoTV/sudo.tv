a7ba025424861731adf72aed79117b21d1c6c0b1
WMXPY <wm@mengw.io>
2021-12-30 17:32:25 -0600

    use common modules

--- assets/js/language-footer.js
+++
@@ -1,9 +0,0 @@
-(() => {
-
-    document.getElementById('language-select').value = getLanguage();
-
-    document.getElementById('language-select').addEventListener('change', (event) => {
-
-        setLanguage(event.target.value);
-    });
-})();
